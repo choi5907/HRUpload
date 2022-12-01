60a26bf37254061e51e902936349bd4045b151d4
choi5907 <99639267+choi5907@users.noreply.github.com>
2022-12-01 17:12:37 +0900

    1201

---
+++ Assets/01.Scene/ZC/ZomMov.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZomMov : MonoBehaviour
+{
+    // Starting State
+    private void HandleStateMachine(){
+        // Run logic, based on which state we are currently in
+        // If logic is met to switch to the next state, we change states
+    }
+}
